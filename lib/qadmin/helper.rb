b6c5e4c26cc96ab1a73d18f8b8ba9ffbd724b9f1
Aaron Quint <aaron@quirkey.com>
2009-01-28 11:52:36 -0500

    Fixing sorterable header misname

--- lib/qadmin/helper.rb
+++ lib/qadmin/helper.rb
@@ -53,8 +53,8 @@ module Qadmin
       end
     end
     
-    def sortable_column_header(attribute, text = nil, options = {})
-      link_text = text || self.qadmin_configuration.column_headers[attribute] || attribute.to_s.humanize
+    def sortable_column_header(attribute_name, text = nil, options = {})
+      link_text = text || self.qadmin_configuration.column_headers[attribute_name] || attribute_name.to_s.humanize
       return link_text unless qadmin_configuration.model_klass.can_query?
       query_parser = model_restful_query_parser(options)
       query_param = options[:query_param] || :query
