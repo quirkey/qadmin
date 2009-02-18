41ba9d8e682eaeec1221e6828d8a84fb4479b82c
Aaron Quint <aaron@quirkey.com>
2009-02-18 16:22:59 -0500

    Added text_field_with_hint builder

--- lib/qadmin/form_builder.rb
+++ lib/qadmin/form_builder.rb
@@ -6,6 +6,18 @@ module Qadmin
       locals = options.reverse_merge({:content_type => object_name, :content => object, :f => self, :options => options})
       @template.render(:partial => "content_forms/#{form_name}_form", :locals => locals)
     end
-    
+   
+    def text_field_with_hint(method, options = {})
+      if object.send(method).blank?
+        options[:class] = if options[:class]
+          options[:class] << ' hinted'
+        else 
+          'hinted'
+        end
+        options[:value] = options.delete(:hint)
+      end
+      text_field(method, options)
+    end
+   
   end
 end
