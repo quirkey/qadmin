eb8e629f2666d8441d5db93f35c27bc6d8ae2591
Aaron Quint <aaron@quirkey.com>
2009-10-27 13:23:50 -0400

    Removing unused content_form stuff until i can make it more factorable

--- lib/qadmin/form_builder.rb
+++ lib/qadmin/form_builder.rb
@@ -1,11 +1,5 @@
 module Qadmin
   class FormBuilder < ::ActionView::Helpers::FormBuilder
-    include Qadmin::Assets::FormBuilder
-    
-    def content_form(form_name, options = {})
-      locals = options.reverse_merge({:content_type => object_name, :content => object, :f => self, :options => options})
-      @template.render(:partial => "content_forms/#{form_name}_form", :locals => locals)
-    end
    
     def text_field_with_hint(method, options = {})
       if object.send(method).blank?
