1bcae6354a6ff8a069c35e30528e88c266824e55
Samuel Elliott <sam@lenary.co.uk>
2010-01-10 21:23:09 +0000

    Updated helper.rb so that admin_table can use row action sets

--- lib/qadmin/helper.rb
+++ lib/qadmin/helper.rb
@@ -1,18 +1,8 @@
-module Qadmin  
-  module Helper    
+module Qadmin
+  module Helper
 
-    def admin_controls(name, options = {}, &block)
-      return if respond_to?(:overlay?) && overlay?
-      controller     = options[:controller] || name.to_s.tableize
-      assumed_object = self.instance_variable_get "@#{name}"
-      logger.debug "= admin_controls: name : #{name}, assumed_object: #{assumed_object.inspect}"
-      obj            = options[:object] || assumed_object || nil
-      parent         = options[:parent] || false
-
-      parent_link_attributes = parent ? {parent.class.to_s.foreign_key => parent.id} : {}
-      general_link_attributes = {:controller => controller}.merge(parent_link_attributes)
-
-      control_links = {
+    def control_links(more_links={})
+      {
         :index      => lambda { link_to(image_tag('qadmin/icon_list.png') + " Back to List", general_link_attributes.merge(:action => 'index')) },
         :new        => lambda { link_to(image_tag('qadmin/icon_new.png') + " New", general_link_attributes.merge(:action => 'new')) },
         :edit       => lambda { link_to(image_tag('qadmin/icon_edit.png') + " Edit", general_link_attributes.merge({:action => 'edit', :id => obj.id})) },
@@ -22,7 +12,19 @@ module Qadmin
         :export     => lambda { link_to(image_tag('qadmin/icon_export.png') + " Export", general_link_attributes.merge(:action => 'export')) },
         :preview    => lambda { link_to(image_tag('qadmin/icon_preview.png') + " Preview", general_link_attributes.merge({:action => 'preview', :id => obj.id})) },
         :sort       => lambda { link_to(image_tag('qadmin/icon_sort.png') + " Sort", general_link_attributes.merge({:action => 'sort'})) }
-      }
+      }.merge(more_links)
+    end
+
+    def admin_controls(name, options = {}, &block)
+      return if respond_to?(:overlay?) && overlay?
+      controller     = params[:controller] || options[:controller] || name.to_s.tableize
+      assumed_object = self.instance_variable_get "@#{name}"
+      logger.debug "= admin_controls: name : #{name}, assumed_object: #{assumed_object.inspect}"
+      obj            = options[:object] || assumed_object || nil
+      parent         = options[:parent] || false
+
+      parent_link_attributes = parent ? {parent.class.to_s.foreign_key => parent.id} : {}
+      general_link_attributes = {:controller => controller}.merge(parent_link_attributes)
 
       control_sets = {
         :index => [:new],
@@ -30,17 +32,17 @@ module Qadmin
         :edit  => [:index,:new,:show,:destroy],
         :show  => [:index,:new,:edit,:destroy]
       }
-      
+
       control_set = (options[:controls] || []).dup
       control_set.unshift(control_sets[options[:for]]) if options[:for]
       control_set << :ports if options[:ports]
-      controls = [control_set].flatten.collect {|c| control_links[c] }.compact
+      controls = [control_set].flatten.collect {|c| control_links(options[:control_links])[c] }.compact
 
       html = ""
       html << %{<ul class="admin_controls">}
-      controls.each do |control| 
+      controls.each do |control|
         control_html = control.respond_to?(:call) ? control.call : control
-        html << li(control_html) 
+        html << li(control_html)
       end
       if block_given?
         html << capture(&block)
@@ -51,10 +53,10 @@ module Qadmin
         html
       end
     end
-    
+
     def sortable_column_header(attribute_name, text = nil, options = {})
       link_text = text || self.qadmin_configuration.on_index.column_headers[attribute_name] || attribute_name.to_s.humanize
-      return link_text unless qadmin_configuration.model_klass.can_query?      
+      return link_text unless qadmin_configuration.model_klass.can_query?
       query_parser = model_restful_query_parser(options)
       if qadmin_configuration.model_klass.respond_to?(:reflections) and
         association = qadmin_configuration.model_klass.reflections[attribute_name.to_sym]
@@ -71,36 +73,54 @@ module Qadmin
       query_parser.set_sort(attribute_name, sorting_this ? sorting_this.next_direction : 'desc')
       link_to link_text, self.params.dup.merge(query_param => query_parser.to_query_hash, :anchor => (options[:id] || self.qadmin_configuration.model_collection_name)), :class => 'sortable_column_header'
     end
-    
+
     def model_restful_query_parser(options = {})
       query_param = options[:query_param] || :query
       qadmin_configuration.model_klass.restful_query_parser(params[query_param], options)
     end
-        
+
+    def row_action_links(more_links={})
+      {
+        :destroy    => lambda { link_to(image_tag("qadmin/icon_destroy.png"), general_link_attributes.merge({:action => 'destroy', :id => obj.id}), :confirm 'Are you sure?', :method => :delete)}},
+        :edit       => lambda { link_to(image_tag('qadmin/icon_edit.png'),    general_link_attributes.merge({:action => 'edit',    :id => obj.id})) },
+        :show       => lambda { link_to(image_tag('qadmin/icon_show.png'),    general_link_attributes.merge({:action => 'show',    :id => obj.id})) },
+      }.merge(more_links)
+    end
+
     def admin_table(collection, options = {})
       config = self.qadmin_configuration.on_index
-      controller  = options[:controller]  || config.controller_name
-      attributes  = options[:attributes]  || config.columns 
+      controller  = prams[:controller]   || options[:controller] || config.controller_name
+      attributes  = options[:attributes] || config.columns
+
+      row_action_sets = {
+        :index => [:show,:edit,:destroy]
+      }
+
       row_actions = options[:row_actions] || config.row_actions
-      
+      row_actions.unshift(row_action_sets[options[:for]]) if options[:for]
+      row_controls = [row_actions].flatten.collect{|a| row_action_links(options[:row_links])[a]}.compact
+
+      parent_link_attributes = parent ? {parent.class.to_s.foreign_key => parent.id} : {}
+      general_link_attributes = {:controller => controller}.merge(parent_link_attributes)
+
       attribute_handlers = HashWithIndifferentAccess.new({
-        :string  =>  lambda {|h, v, i, c| 
+        :string  =>  lambda {|h, v, i, c|
           auto_link(h(v))
         },
         :boolean =>  lambda {|h, v, i, c| yes?(v) },
         :false_class => lambda {|h, v, i, c| 'No' },
         :true_class => lambda {|h, v, i, c| 'Yes' },
-        :text    =>  lambda {|h, v, i, c| 
-          h.truncate(v, :length => 30, :omission => "... #{h.link_to('More', h.send("#{c.model_instance_name}_path", i))}") 
+        :text    =>  lambda {|h, v, i, c|
+          h.truncate(v, :length => 30, :omission => "... #{h.link_to('More', h.send("#{c.model_instance_name}_path", i))}")
          },
         :reflection => lambda {|h, v, i, c| h.link_to(v.to_s, v) },
-        :hash => lambda {|h, v, i, c| v.inspect }, 
-        :array => lambda {|h, v, i, c| v.inspect } 
-      })     
+        :hash => lambda {|h, v, i, c| v.inspect },
+        :array => lambda {|h, v, i, c| v.inspect }
+      })
       attribute_handlers.merge!(options[:attribute_handlers] || config.attribute_handlers)
       logger.debug "attribute_handlers #{attribute_handlers.inspect}"
       model_column_types = HashWithIndifferentAccess.new
-      
+
       attributes.each do |attribute_name|
         if column = config.model_klass.columns.detect {|c| c.name.to_s == attribute_name.to_s }
           if serialized_klass = config.model_klass.serialized_attributes[attribute_name]
@@ -113,7 +133,7 @@ module Qadmin
         end
         model_column_types[attribute_name] = column if column
       end
-      
+
       html = "<table id=\"#{options[:id] || config.model_collection_name}\">"
       html <<	'<thead><tr>'
       attributes.each_with_index do |attribute, i|
@@ -143,9 +163,9 @@ module Qadmin
           css = (config.column_css[attribute] ? config.column_css[attribute] : (i == 0 ? 'first_col' : ''))
           html << %{<td class="#{css}">#{value}</td>}
         end
-        row_actions.each do |action|
-          http_method = (action.to_sym == :destroy) ? :delete : :get
-          html << %{<td>#{link_to(image_tag("qadmin/icon_#{action}.png"), {:controller => controller, :action => action, :id => instance.id}, :method => http_method)}</td>}
+        row_controls.each do |control|
+          control_html = control.respond_to?(:call) ? control.call : action
+          html << td(control_html)
         end
         html << '</tr>'
       end
@@ -155,13 +175,17 @@ module Qadmin
     def alt_rows
       %{class="#{cycle('alt', '')}"}
     end
-    
+
     def yes?(boolean)
       boolean ? 'Yes' : 'No'
     end
-    
+
     def li(content, *args)
       content_tag :li, content, args
+    end
+
+    def td(content, *args)
+      content_tag :td, content, args
     end
 
     def simple_admin_menu(*controllers)
@@ -188,7 +212,7 @@ module Qadmin
       end
       html
     end
-    
+
     def like_current_page?(options)
       case options
       when Hash
@@ -201,7 +225,7 @@ module Qadmin
       request = @controller.request
       request.request_uri =~ url_string
     end
-    
+
     def fieldset(legend = nil, options = {}, &block)
       concat(content_tag(:fieldset, options) do
         html = ''
@@ -210,17 +234,17 @@ module Qadmin
         html
       end)
     end
-    
+
     def labeled_show_column(object, method, options = {})
       html = %{<p>}
       html << label(object, method, options[:label] || nil)
-      show_value = options.has_key?(:value) ? options[:value] : object.send(method) 
+      show_value = options.has_key?(:value) ? options[:value] : object.send(method)
       show_value = '<em>blank</em>' if show_value.blank?
       html << %{<span class="show_value">#{show_value}</span>}
       html << %{</p>}
       html
     end
-    
+
     def labeled_show_paperclip_attachment(object, method, options = {})
       value = if object.send("#{method}?")
         # has attachment
