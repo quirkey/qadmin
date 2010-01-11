66dc5c0effa71bc5898e0dbc88221e0efc0ed2df
Samuel Elliott <sam@lenary.co.uk>
2010-01-11 18:44:11 +0100

    finally fixed it. it properly works now!

--- lib/qadmin/helper.rb
+++ lib/qadmin/helper.rb
@@ -38,6 +38,8 @@ module Qadmin
       control_set << :ports if options[:ports]
       controls = [control_set].flatten.collect {|control| control_links(options[:control_links])[control] }.compact
 
+      logger.debug 'control_set: ' + control_set.inspect
+
       html = ""
       html << %{<ul class="admin_controls">}
       controls.each do |control|
@@ -81,9 +83,9 @@ module Qadmin
 
     def row_control_links(more_links={})
       {
-        :destroy    => lambda { link_to(image_tag("qadmin/icon_destroy.png"), general_link_attributes.merge({:action => 'destroy', :id => instance.id}), :confirm => 'Are you sure?', :method => :delete)},
-        :edit       => lambda { link_to(image_tag('qadmin/icon_edit.png'),    general_link_attributes.merge({:action => 'edit',    :id => instance.id})) },
-        :show       => lambda { link_to(image_tag('qadmin/icon_show.png'),    general_link_attributes.merge({:action => 'show',    :id => instance.id})) },
+        :destroy    => lambda { |obj| link_to(image_tag("qadmin/icon_destroy.png"), {:action => 'destroy', :id => obj.id}, :confirm => 'Are you sure?', :method => :delete)},
+        :edit       => lambda { |obj| link_to(image_tag('qadmin/icon_edit.png'),    {:action => 'edit',    :id => obj.id}) },
+        :show       => lambda { |obj| link_to(image_tag('qadmin/icon_show.png'),    {:action => 'show',    :id => obj.id}) },
       }.merge(more_links)
     end
 
@@ -91,18 +93,16 @@ module Qadmin
       config = self.qadmin_configuration.on_index
       controller  = params[:controller]  || options[:controller] || config.controller_name
       attributes  = options[:attributes] || config.columns
-      parent      = options[:parent] || false
 
       row_control_sets = {
         :index => [:show,:edit,:destroy]
       }
 
-      row_control_set = options[:row_controls] || config.row_controls
+      row_control_set = (options[:row_controls] || config.row_controls).dup
       row_control_set.unshift(row_control_sets[options[:for]]) if options[:for]
       row_controls = [row_control_set].flatten.collect{|c| row_control_links(options[:row_control_links])[c] }.compact
 
-      parent_link_attributes = parent ? {parent.class.to_s.foreign_key => parent.id} : {}
-      general_link_attributes = {:controller => controller}.merge(parent_link_attributes)
+      logger.debug 'row_control_set: ' + row_control_set.inspect
 
       attribute_handlers = HashWithIndifferentAccess.new({
         :string  =>  lambda {|h, v, i, c|
@@ -164,7 +164,7 @@ module Qadmin
           css = (config.column_css[attribute] ? config.column_css[attribute] : (i == 0 ? 'first_col' : ''))
           html << %{<td class="#{css}">#{value}</td>}
         end
-        row_control_links.each do |row_control|
+        row_controls.each do |row_control|
           row_control_html = row_control.respond_to?(:call) ? row_control.call(instance) : row_control
           html << td(row_control_html)
         end
