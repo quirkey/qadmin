64a782fb3eb6fd682dc7bb53664e3d57710e3b36
arirusso <ari.russo@gmail.com>
2015-02-26 18:42:19 -0500

    ease off the module eval and such

--- lib/qadmin/configuration.rb
+++ lib/qadmin/configuration.rb
@@ -4,96 +4,58 @@ require 'active_support/core_ext/string/inflections'
 module Qadmin
   module Configuration
 
-    # Turn a set of options into the full options needed for configuration
-    def self.extract_model_from_options(options = {})
-      output = {}
-      output[:controller_klass]      = options[:controller_klass]
-      output[:controller_name]       = options[:controller_name] || output[:controller_klass].to_s.demodulize.gsub(/Controller/,'').underscore
-      output[:model_name]            = options[:model_name] || output[:controller_klass].to_s.demodulize.gsub(/Controller/,'').singularize
-      output[:model_instance_name]   = options[:model_instance_name] || output[:model_name].underscore
-      output[:model_collection_name] = options[:model_collection_name] || output[:model_instance_name].pluralize
-      output[:model_human_name]      = options[:model_human_name] || output[:model_instance_name].humanize
-
-      possible_namespace = output[:controller_klass].to_s.underscore.split('/')[0]
-      output[:namespace] = options[:namespace] || (possible_namespace =~ /controller/) ? nil : possible_namespace.to_sym
-      output
-    end
-
     class Base < ::HashWithIndifferentAccess
 
-      attr_accessor :base
+      attr_reader :base,
+                  :controller_class,
+                  :controller_name,
+                  :model_class,
+                  :model_name,
+                  :model_instance_name,
+                  :model_collection_name,
+                  :model_human_name,
+                  :namespace,
+                  :parent,
+                  :default_scope
 
       def with_indifferent_access
         self
       end
 
-      def self.hash_accessor(name, options = {})
-        options[:default] ||= nil
-        coerce = options[:coerce] ? ".#{options[:coerce]}" : ""
-        module_eval <<-EOT
-          def #{name}
-            value = (self[:#{name}] ? self[:#{name}]#{coerce} : self[:#{name}]) ||
-                      (base && base.respond_to?(:#{name}) ? base.send(:#{name}) : #{options[:default].inspect})
-            yield value if block_given?
-            value
-          end
-
-          def #{name}=(value)
-            self[:#{name}] = value
-          end
-
-          def #{name}?
-            !!self[:#{name}]
-          end
-        EOT
-      end
-
-      hash_accessor :controller_klass
-      hash_accessor :controller_name
-      hash_accessor :model_name
-      hash_accessor :model_instance_name
-      hash_accessor :model_collection_name
-      hash_accessor :model_human_name
-      hash_accessor :namespace, :default => false
-      hash_accessor :parent, :default => false
-      hash_accessor :default_scope, :default => false
-
       def initialize(options = {})
         super
         @base = options.delete(:base)
-      end
-
-      def model_klass
-        @model_klass ||= self.model_name.constantize
+        @namespace = false
+        @parent = false
+        @default_scope = false
+        populate_model(options)
       end
 
       def path_prefix(plural = false)
-        name = plural ? model_collection_name : model_instance_name
-        if namespace
-          "#{namespace}_#{name}"
-        else
+        name = plural ? @model_collection_name : @model_instance_name
+        if @namespace.nil?
           name
+        else
+          "#{@namespace}_#{name}"
         end
       end
 
       def polymorphic_array(*args)
         args.compact!
-        if parent && args.length < 2
-          args.unshift parent
+        if @parent && args.length < 2
+          args.unshift(@parent)
         end
-        if namespace
-          args.unshift namespace
-        end
+        args.unshift(@namespace) if @namespace
         args
       end
 
       def form_instance_for(instance)
-        i = instance.class != model_klass ? instance.becomes(model_klass) : instance
+        i = instance.class != @model_class ? instance.becomes(@model_class) : instance
         polymorphic_array(i)
       end
 
       def model_column_names
-        @columns ||= model_klass.column_names
+        @columns ||= @model_class.column_names
       rescue
         []
       end
@@ -102,41 +64,92 @@ module Qadmin
         "#<#{self.class} #{super}>"
       end
 
-      protected
+      private
+
+      # Turn a set of options into the full options needed for configuration
+      def populate_model(properties)
+        @controller_class = properties[:controller_class]
+        @controller_name = properties[:controller_name] || @controller_class.to_s.demodulize.gsub(/Controller/,'').underscore
+        @model_name = properties[:model_name] || @controller_class.to_s.demodulize.gsub(/Controller/,'').singularize
+        @model_instance_name = properties[:model_instance_name] || @model_name.underscore
+        @model_collection_name = properties[:model_collection_name] || @model_instance_name.pluralize
+        @model_human_name = properties[:model_human_name] || @model_instance_name.humanize
+        populate_namespace(properties[:namespace])
+        @model_class = @model_name.constantize
+        self
+      end
+
+      def populate_namespace(namespace)
+        if match = @controller_class.to_s.match(/\A(.+)::\w+\z/)
+          modules = match[1]
+          @namespace = modules.underscore unless modules.nil?
+        end
+        @namespace ||= namespace
+        @namespace = @namespace.to_sym unless @namespace.nil?
+      end
+
     end
 
     module Actions
       class Action < Qadmin::Configuration::Base
-        hash_accessor :multipart_forms, :default => false
-        hash_accessor :controls, :default => []
-        hash_accessor :control_links, :default => {}
-      end
-
-      class Index < Action
-        hash_accessor :columns, :default => []
-        hash_accessor :column_headers, :default => {}
-        hash_accessor :column_css, :default => {}
-        hash_accessor :controls, :default => [:new]
-        hash_accessor :row_controls, :default => [:show, :edit, :destroy]
-        hash_accessor :attribute_handlers, :default => {}
+        attr_reader :multipart_forms,
+                    :controls,
+                    :control_links
 
         def initialize(options = {})
           super
-          self.columns = model_column_names
+          @multipart_forms = false
+          @controls = []
+          @control_links = {}
+        end
+
+      end
+
+      class Index < Action
+        attr_reader :columns,
+                    :column_headers,
+                    :column_css,
+                    :controls,
+                    :row_controls,
+                    :attribute_handlers
+
+        def initialize(options = {})
+          super
+          @columns = model_column_names
+          @column_headers = {}
+          @column_css = {}
+          @controls = [:new]
+          @row_controls = [:show, :edit, :destroy]
+          @attribute_handlers = {}
         end
 
       end
 
       class Show < Action
-        hash_accessor :controls, :default => [:index, :new, :edit, :destroy]
+
+        def initialize(options = {})
+          super
+          @controls = [:index, :new, :edit, :destroy]
+        end
+
       end
 
       class New < Action
-        hash_accessor :controls, :default => [:index]
+
+        def initialize(options = {})
+          super
+          @controls = [:index]
+        end
+
       end
 
       class Edit < Action
-        hash_accessor :controls, :default => [:index, :new, :show, :destroy]
+
+        def initialize(options = {})
+          super
+          @controls = [:index, :new, :show, :destroy]
+        end
+
       end
 
       class Create < Action
@@ -157,35 +170,34 @@ module Qadmin
 
       ACTIONS = [:index, :show, :new, :create, :edit, :update, :destroy].freeze
 
-      hash_accessor :available_actions, :default => ACTIONS.dup
-      hash_accessor :ports, :default => false
-
-      hash_accessor :multipart_forms, :default => false
-      hash_accessor :control_links, :default => {}
+      attr_accessor :available_actions
+      attr_reader :ports,
+                  :multipart_forms,
+                  :control_links
 
       def initialize(options = {})
         super
-        self.update(Qadmin::Configuration.extract_model_from_options(options))
+        @available_actions = options[:available_actions] || ACTIONS.dup
+        @ports = false
+        @multipart_forms = false
+        @control_links = {}
+        @actions = {}
+        populate_model(options)
       end
 
-      ACTIONS.each do |action|
-        hash_accessor "on_#{action}"
-
-        module_eval <<-EOV
-          def on_#{action}
-            value = self["on_#{action}"] ||= "Qadmin::Configuration::Actions::#{action.to_s.classify}".constantize.new(self.clean_self.merge(:base => self))
-            yield value if block_given?
-            value
+      def method_missing(method, *args, &block)
+        if match = method.to_s.match(/\Aon\_(\w+)/)
+          action = match[1]
+          if @available_actions.include?(action.to_sym) && (value = @actions[action]).nil?
+            klass = "Qadmin::Configuration::Actions::#{action.to_s.classify}".constantize
+            value = klass.new(:controller_class => @controller_class)
+            @actions[action] = value
           end
-        EOV
-      end
-
-      # We need to provide just the "own" properties for the other actions to inherit
-      # so that its not a crazy self referential mess
-      def clean_self
-        c = {}
-        self.each {|k, v| c[k] = v if k !~ /^on_/ }
-        c
+          yield(value) if block_given?
+          value
+        else
+          super
+        end
       end
 
     end
