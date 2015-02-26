f70a56a2f53a2ff3ebfbe8edce0a50e2f7a3293c
arirusso <ari.russo@gmail.com>
2015-02-26 18:43:47 -0500

    initialize properly

--- test/configuration_test.rb
+++ test/configuration_test.rb
@@ -3,16 +3,25 @@ require "helper"
 class Qadmin::ConfigurationTest < Minitest::Test
 
   context "Configuring a Resource" do
-    setup do
-      @configuration = Qadmin::Configuration::Resource.new(:controller_klass => ThingsController)
-    end
 
-    context "initializing" do
-      should "extrapolate the model klass names" do
+    context "root namespace" do
+
+      setup do
+        class ::Thing
+        end
+        @configuration = Qadmin::Configuration::Resource.new(:controller_class => ThingsController)
+      end
+
+      should "extrapolate the model info" do
         assert_nil @configuration.namespace
         assert_equal "Thing", @configuration.model_name
         assert_equal "Thing", @configuration.model_human_name
         assert_equal "things", @configuration.model_collection_name
+      end
+
+      should "have no namespace" do
+        assert_equal 'thing', @configuration.path_prefix
+        assert_equal 'thing', @configuration.on_create.path_prefix
       end
 
       should "create special config objects for each action" do
@@ -21,28 +30,32 @@ class Qadmin::ConfigurationTest < Minitest::Test
         end
       end
 
-      should "extract the namespace from a namespaced controller" do
-        @configuration = Qadmin::Configuration::Resource.new(:controller_klass => Admin::ItemsController)
+    end
+
+    context "admin namespace" do
+
+      setup do
+        module ::Admin
+          class Item
+          end
+        end
+        @configuration = Qadmin::Configuration::Resource.new(:controller_class => ::Admin::ItemsController)
+      end
+
+      should "populate the model info" do
         assert_equal :admin, @configuration.namespace
         assert_equal "Item", @configuration.model_name
         assert_equal "Item", @configuration.model_human_name
         assert_equal "items", @configuration.model_collection_name
       end
 
-    end
-
-    context "path_prefix" do
-      should "include namespace if set" do
-        @configuration.namespace = :admin
-        assert_equal 'admin_thing', @configuration.path_prefix
-        assert_equal 'admin_thing', @configuration.on_create.path_prefix
+      should "include path prefix" do
+        assert_equal 'admin_item', @configuration.path_prefix
+        assert_equal 'admin_item', @configuration.on_create.path_prefix
       end
 
-      should "not include nil namespace" do
-        assert_equal 'thing', @configuration.path_prefix
-        assert_equal 'thing', @configuration.on_create.path_prefix
-      end
     end
+
   end
 
 end
