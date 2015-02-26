95410faecf6401b809d964d963fbabd153d0993a
arirusso <ari.russo@gmail.com>
2015-02-26 14:52:05 -0500

    required for rails 2

--- lib/qadmin.rb
+++ lib/qadmin.rb
@@ -1,6 +1,8 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
+require 'iconv'
+
 unless defined?(ActiveSupport)
   require 'active_support'
 end
