a1065a80430c9b4444856f7553cfe69448918be9
arirusso <ari.russo@gmail.com>
2016-01-14 17:30:57 -0500

    conditional super

--- lib/qadmin/templates.rb
+++ lib/qadmin/templates.rb
@@ -41,13 +41,19 @@ module Qadmin
     def current_section_name(options = {})
       options[:section_name] ? options[:section_name] : (@section ? @section.name : controller_path)
     end
-    
+
     def template_exists?(template_path)
-      logger.debug "Checking for template: #{template_path}"
-      self.view_paths.find_template(template_path)
-    rescue ActionView::MissingTemplate
-      logger.debug "Template not found: #{template_path}"
-      false
+      if defined?(super)
+        super
+      else
+        begin
+          logger.debug "Checking for template: #{template_path}"
+          self.view_paths.find_template(template_path)
+        rescue ActionView::MissingTemplate
+          logger.debug "Template not found: #{template_path}"
+          false
+        end
+      end
     end
 
   end
