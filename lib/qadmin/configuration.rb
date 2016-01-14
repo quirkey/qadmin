5cee4c990363f8fc619ce7fe1550895fc032572b
arirusso <ari.russo@gmail.com>
2016-01-14 11:37:20 -0500

    restore

--- lib/qadmin/configuration.rb
+++ lib/qadmin/configuration.rb
@@ -1,35 +1,33 @@
 module Qadmin
   module Configuration
-    
+
     class Base < ::HashWithIndifferentAccess
-      
+
       attr_accessor :base
-      
+
       def with_indifferent_access
         self
       end
-      
+
       def self.hash_accessor(name, options = {})
-        options[:default] ||= nil 
+        options[:default] ||= nil
         coerce = options[:coerce] ? ".#{options[:coerce]}" : ""
         module_eval <<-EOT
           def #{name}
-            value = (self[:#{name}] ? self[:#{name}]#{coerce} : self[:#{name}]) || 
+            value = (self[:#{name}] ? self[:#{name}]#{coerce} : self[:#{name}]) ||
                       (base && base.respond_to?(:#{name}) ? base.send(:#{name}) : #{options[:default].inspect})
             yield value if block_given?
             value
           end
-
           def #{name}=(value)
             self[:#{name}] = value
           end
-
           def #{name}?
             !!self[:#{name}]
           end
         EOT
       end
-      
+
       hash_accessor :controller_klass
       hash_accessor :controller_name
       hash_accessor :model_name
@@ -39,7 +37,7 @@ module Qadmin
       hash_accessor :namespace, :default => false
       hash_accessor :parent, :default => false
       hash_accessor :default_scope, :default => false
-      
+
       def initialize(options = {})
         super
         @base = options.delete(:base)
@@ -49,7 +47,7 @@ module Qadmin
       def model_klass
         self.model_name.constantize
       end
-      
+
       def path_prefix(plural = false)
         name = plural ? model_collection_name : model_instance_name
         if namespace
@@ -58,7 +56,7 @@ module Qadmin
           name
         end
       end
-      
+
       def form_instance_for(instance)
         if parent
           [parent, instance]
@@ -66,28 +64,28 @@ module Qadmin
           [namespace, instance]
         else
           instance
-        end 
+        end
       end
-      
+
       def model_column_names
         model_klass.column_names
       rescue
         []
       end
-      
+
       def inspect
         "#<#{self.class} #{super}>"
       end
-      
+
       protected
       def extract_model_from_options(options = {})
         self.controller_klass      = options[:controller_klass]
         self.controller_name       = options[:controller_name] || controller_klass.to_s.demodulize.gsub(/Controller/,'').underscore
         self.model_name            = options[:model_name] || controller_klass.to_s.demodulize.gsub(/Controller/,'').singularize
         self.model_instance_name   = options[:model_instance_name] || model_name.underscore
-        self.model_collection_name = options[:model_collection_name] || model_instance_name.pluralize    
+        self.model_collection_name = options[:model_collection_name] || model_instance_name.pluralize
         self.model_human_name      = options[:model_human_name] || model_instance_name.humanize
-        
+
         possible_namespace = controller_klass.to_s.underscore.split('/')[0]
         self.namespace = options[:namespace] || (possible_namespace =~ /controller/) ? nil : possible_namespace.to_sym
       end
@@ -97,21 +95,21 @@ module Qadmin
       class Action < Qadmin::Configuration::Base
         hash_accessor :multipart_forms, :default => false
         hash_accessor :controls, :default => []
-        
+
       end
-      
+
       class Index < Action
         hash_accessor :columns, :default => []
         hash_accessor :column_headers, :default => {}
         hash_accessor :column_css, :default => {}
         hash_accessor :row_controls, :default => [:show, :edit, :destroy]
         hash_accessor :attribute_handlers, :default => {}
-        
+
         def initialize(options = {})
           super
           self.columns = model_column_names
         end
-        
+
       end
 
       class Show < Action
@@ -127,7 +125,7 @@ module Qadmin
       end
 
       class Create < Action
-        
+
       end
 
       class Update < Action
@@ -137,22 +135,22 @@ module Qadmin
       class Destroy < Action
 
       end
-      
+
     end
 
     class Resource < Base
-      
+
       ACTIONS = [:index, :show, :new, :create, :edit, :update, :destroy].freeze
-      
+
       hash_accessor :available_actions, :default => ACTIONS.dup
       hash_accessor :ports, :default => false
-      
+
       hash_accessor :multipart_forms, :default => false
       hash_accessor :controls, :default => []
-      
+
       ACTIONS.each do |action|
         hash_accessor "on_#{action}"
-      
+
         module_eval <<-EOV
           def on_#{action}
             value = self["on_#{action}"] ||= "Qadmin::Configuration::Actions::#{action.to_s.classify}".constantize.new(self.dup.merge(:base => self))
@@ -161,7 +159,7 @@ module Qadmin
           end
         EOV
       end
-            
+
     end
 
   end
