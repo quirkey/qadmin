8a585f5266ca02fe857f98a52634ca252f822492
arirusso <ari.russo@gmail.com>
2016-03-01 13:22:55 -0500

    use modules

--- lib/qadmin/configuration.rb
+++ lib/qadmin/configuration.rb
@@ -113,7 +113,7 @@ module Qadmin
 
       def populate_accessors
         self.class.hash_accessors[self.class.name].each do |accessor|
-          p "**" if accessor.to_s == "attribute_handlers" && respond_to?(accessor)
+          p send(accessor) if accessor.to_s == "controls" && respond_to?(accessor)
           send(accessor) if respond_to?(accessor)
         end
       end
